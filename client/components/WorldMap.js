25dd08f3386e191fe8e02fe8ce072fd3f1c0a5cd
Erika Newland <erika.s.newland@gmail.com>
2017-09-25 12:13:56 -0400

    feat: leaflet works except zoom functionality

--- client/components/WorldMap.js
+++ client/components/WorldMap.js
@@ -5,9 +5,9 @@ import { interpolateHclLong } from 'd3-interpolate'
 import { max, min } from 'd3-array'
 import { connect } from 'react-redux'
 import countryCodes from '../../data/countryCode'
-// import * as d3 from 'd3'
+import * as d3 from 'd3'
 import { transition } from 'd3-transition'
-import { geoMercator, geoPath, geoTransform } from 'd3-geo'
+import { geoMercator, geoPath, geoTransform, geoStream } from 'd3-geo'
 import { Map, TileLayer } from 'react-leaflet'
 import L from 'leaflet'
 
@@ -19,20 +19,43 @@ export default class WorldMap extends Component {
 
 
   renderMap(tweetData, geoData) {
+    console.log('stream', geoStream)
     const node = this.node
     const map = node.leafletElement
 
     const svg = select(map.getPanes().overlayPane).append("svg")
     const g = svg.append("g").attr("class", "leaflet-zoom-hide");
 
-    tweetData.location.LatLng = new L.LatLng(tweetData.location.lat, tweetData.location.long)
-    
-    function projectPoint(x, y) {
-			const point = map.latLngToLayerPoint(new L.LatLng(y, x));
-			this.stream.point(point.x, point.y);
-		}
+    // if(tweetData.location.lat === "D.C.") {
+    //   tweetData.location.lat = 38.9072
+    //   tweetData.location.long = 77.0369
+    // }
 
-    const transform = geoTransform({point: projectPoint})
+    let box = {
+      "type": "FeatureCollection",
+      "features": [{
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[
+            [tweetData.lat + 1, tweetData.long + 1],
+            [tweetData.lat + 1, tweetData.long - 1],
+            [tweetData.lat - 1, tweetData.long + 1],
+            [tweetData.lat - 1, tweetData.long - 1],
+          ]]
+        }
+      }
+      ]
+    }
+
+    tweetData.location.LatLng = new L.LatLng(tweetData.location.lat, tweetData.location.long)
+
+    function projectPoint(x, y) {
+      const point = map.latLngToLayerPoint(tweetData.location.LatLng);
+      this.stream.point(point.x, point.y);
+    }
+
+    const transform = geoTransform({ point: projectPoint })
     const path = geoPath().projection(transform);
 
     console.log('tweetData', tweetData.location)
@@ -41,27 +64,32 @@ export default class WorldMap extends Component {
       .enter()
       .append("circle")
       .classed('city', true)
-      .attr("r", "8px")
+      .attr("r", "4px")
       .attr("fill", "red")
-    // .transition()  //may need to move this to update
-    // .delay(1000)
-    // .remove()
+      .transition()
+      .delay(1000)
+      .duration(1000)
+      .remove()
+      
+  
 
     map.on("viewreset", update);
     update();
 
     function update() {
-      console.log('path', path)
-      const bounds = path.bounds(tweetData.location.LatLng)  //set bounds here
+      const bounds = path.bounds(box)  //set bounds here
       const topLeft = bounds[0];
       const bottomRight = bounds[1];
+      console.log('bottomRight', bottomRight, "topLeft", topLeft)
 
-      svg.attr("width", bottomRight[0] - topLeft[0])
-        .attr("height", bottomRight[1] - topLeft[1])
+      svg.attr("width", "16px")
+        .attr("height", "16px")
         .style("left", topLeft[0] + "px")
         .style("top", topLeft[1] + "px");
 
-      g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+      g.attr("transform", "translate(" + (-topLeft[0] + 4) + "," + (-topLeft[1] + 4) + ")")
+        .attr("height", "16px")
+        .attr("width", "16px");
 
       circle.attr("transform",
         function (d) {
